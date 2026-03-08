911254c1bd2fbbd74f99b550e99082b1eaf31f21
NeNiao911 <jorky8899@gmail.com>
2026-03-09 02:17:30 +0700

    ini cara commit gimana woi
    masih dalam update
    ketua sudah mau tido
    kelen bikin" dlu lah db nya

---
+++ weeds/index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyCinema</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    body{
+        background: #0f0f0f;
+        min-height: 100vh;
+        display:flex
+        align-items: center;
+        justify-content: center;
+        font-family: system-ui; -apple-system, sans-serif;
+    }
+    .card{
+        background: #1e1e1e;
+        border: none;
+        border-raduis: 1,5rem
+    }
+</body>
+</html>
